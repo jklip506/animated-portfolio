0281135f9139b0e81605d4ec9de5146d922fb3d0
jklip506 <96444138+jklip506@users.noreply.github.com>
2023-11-21 23:07:57 -0600

    Added animated sidebar/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Test from "./Test";
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section><Navbar /></section>
+    <section id="Services">Paralax</section>
+    <section>Services</section>
+    <section id="Portfolio">Paralax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "./sidebar.scss";
+import Links from "./links/Links";
+import ToggleButton from "./toggleButton/ToggleButton";
+
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 30,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.2,
+      type: "spring",
+      stiffness: 200,
+      damping: 20,
+    },
+  },
+};
+
+const Sidebar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen}/>
+    </motion.div>
+  );
+};
+
+export default Sidebar;
